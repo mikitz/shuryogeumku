b99032903694c53fa1169d57767eb0e3091de97f
Mikitz <mfitzgerald06@gmail.com>
2025-11-01 13:20:51 +0900

    dark mode

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,14 @@
+/** @format */
+
 import { Loader2 } from "lucide-react";
 
 export default function Loading() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <Loader2 className="h-12 w-12 animate-spin text-primary" />
-      <p className="mt-4 text-lg text-muted-foreground">Loading...</p>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen dark:bg-gray-950">
+            <Loader2 className="h-12 w-12 animate-spin text-primary dark:text-blue-400" />
+            <p className="mt-4 text-lg text-muted-foreground dark:text-gray-400">
+                Loading...
+            </p>
+        </div>
+    );
 }
-
